78cb63ef17bf031299ede224a119754b4e70924e
Manel Roca <manu2187@hotmail.com>
2015-04-13 17:44:49 +0200

    Initial Commit

---
+++ SOA_Practica/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SOA_Practica
+//
+//  Created by MRR on 13/04/15.
+//  Copyright (c) 2015 MRR. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
